44b77a08f50f628d7b039cf3422adb1e5b6da1b2
Aurelien Essome <aurelien.e@fomosocial.co>
2023-04-10 17:28:16 -0400

    Optimized pausing and scrolling experience.

--- App.js
+++ App.js
@@ -9,10 +9,13 @@ const App = () => {
       uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
     },
     {
-      uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+      uri: 'https://v.pinimg.com/videos/mc/720p/f6/88/88/f68888290d70aca3cbd4ad9cd3aa732f.mp4',
     },
     {
-      uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+      uri: 'https://v.pinimg.com/videos/mc/720p/11/05/2c/11052c35282355459147eabe31cf3c75.mp4',
+    },
+    {
+      uri: 'https://v.pinimg.com/videos/mc/720p/c9/22/d8/c922d8391146cc2fdbeb367e8da0d61f.mp4',
     },
   ];
 
--- FullScreenVideoList.jsx
+++ FullScreenVideoList.jsx
@@ -7,14 +7,66 @@ export default function FullScreenVideoList({ videoList }) {
     const { width, height } = Dimensions.get('window');
 
     const [currentPlayingIndex, setCurrentPlayingIndex] = React.useState(0);
-    const playerRefs = React.useRef([]);
     const flatListRef = React.useRef(null);
+    const playerRefs = React.useRef([]);
+    const threshold = height / 2.5;
+
+    React.useEffect(() => {
+        if (currentPlayingIndex !== null) {
+            const playCurrentVideo = async () => {
+                try {
+                    await playerRefs.current[currentPlayingIndex]?.playAsync();
+                } catch (error) {
+                    console.error('Error playing current video:', error);
+                }
+            };
+
+            const pauseAndResetOtherVideos = async () => {
+                playerRefs.current.forEach(async (player, index) => {
+                    if (index !== currentPlayingIndex) {
+                        try {
+                            await player?.pauseAsync();
+                            if (Math.abs(index - currentPlayingIndex) > 1) { // Check if the video is not adjacent to the currentPlayingIndex
+                                await player?.setPositionAsync(0); // Seek the video to the beginning
+                            }
+                        } catch (error) {
+                            console.error('Error pausing and resetting other videos:', error);
+                        }
+                    }
+                });
+            };
+
+            playCurrentVideo();
+            pauseAndResetOtherVideos();
+        }
+    }, [currentPlayingIndex]);
+
+    const handleScroll = (event) => {
+        const offset = event.nativeEvent.contentOffset.y;
+        const adjustedOffset = offset + threshold;
+        const newIndex = adjustedOffset >= 0 ? Math.floor(adjustedOffset / height) : 0;
+
+        if (currentPlayingIndex !== newIndex) {
+            setCurrentPlayingIndex(newIndex);
+        }
+    };
 
     const renderItem = ({ item, index }) => (
-        <Pressable style={{ width, height, justifyContent: 'center', alignItems: 'center', }} onPressIn={() => playerRefs.current[index]?.pauseAsync()}
-        onPressOut={() => {
+        <Pressable style={{ width, height, justifyContent: 'center', alignItems: 'center', }} 
+        onPressIn={async () => {
+            try {
+                await playerRefs.current[index]?.pauseAsync();
+            } catch (error) {
+                console.error('Error pausing video:', error);
+            }
+        }}
+        onPressOut={async () => {
             if (index === currentPlayingIndex) {
-            playerRefs.current[index]?.playAsync();
+                try {
+                    await playerRefs.current[index]?.playAsync();
+                } catch (error) {
+                    console.error('Error playing video:', error);
+                }
             }
         }}>
         <Video ref={(ref) => (playerRefs.current[index] = ref)} source={{ uri: item.uri }} rate={1.0} volume={1.0} isMuted={false} isLooping resizeMode={ResizeMode.COVER} 
@@ -26,28 +78,10 @@ export default function FullScreenVideoList({ videoList }) {
         </Pressable>
     );
 
-    React.useEffect(() => {
-        if (currentPlayingIndex !== null) {
-        playerRefs.current[currentPlayingIndex]?.playAsync();
-        playerRefs.current.forEach((player, index) => {
-            if (index !== currentPlayingIndex) {
-            player?.pauseAsync().then(() => {
-                player?.setPositionAsync(0); // Seek the video to the beginning
-            });
-            }
-        });
-        }
-    }, [currentPlayingIndex]);
-
-  return (
-    <View style={{ width, height, alignItems: 'center', justifyContent: 'center', }}>
-      <FlatList ref={flatListRef} data={videoList} renderItem={renderItem} keyExtractor={(item, index) => index.toString()} pagingEnabled horizontal={false} showsVerticalScrollIndicator={false} snapToInterval={height} snapToAlignment="start" decelerationRate="fast" 
-      onMomentumScrollEnd={(event) => {
-          const newIndex = Math.round(event.nativeEvent.contentOffset.y / height);
-          if (currentPlayingIndex !== newIndex) {
-            setCurrentPlayingIndex(newIndex);
-          }
-        }} />
-    </View>
-  );
+    return (
+        <View style={{ width, height, alignItems: 'center', justifyContent: 'center', }}>
+            <FlatList ref={flatListRef} data={videoList} renderItem={renderItem} keyExtractor={(item, index) => index.toString()} pagingEnabled horizontal={false} showsVerticalScrollIndicator={false} snapToInterval={height} snapToAlignment="start" decelerationRate={0} 
+            onScroll={handleScroll} />
+        </View>
+    )
 }
